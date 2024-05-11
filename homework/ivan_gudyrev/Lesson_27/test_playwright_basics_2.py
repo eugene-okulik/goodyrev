b363da37a5f1daf94dbc15edd2e230ced2ddaa0f
Ivan <iegudyrev@gmail.com>
2024-05-11 03:22:12 +0300

    fixed lint checks

--- homework/ivan_gudyrev/Lesson_27/test_playwright_basics_2.py
+++ homework/ivan_gudyrev/Lesson_27/test_playwright_basics_2.py
@@ -1,10 +1,6 @@
 from playwright.sync_api import Page, BrowserContext, expect
 
 
-'''
-Напишите тест, который заходит на страницу https://www.qa-practice.com/elements/alert/confirm,
-кликает на кнопку, чтобы появился алерт, жмет Ok и проверяет, что на страние в секции "You selected" написано "Ok"
-'''
 def test_27_part1_alert(page: Page):
     page.set_viewport_size({'width': 1920, 'height': 1080})
     page.on('dialog', lambda alert: alert.accept())
@@ -18,11 +14,6 @@ def test_27_part1_alert(page: Page):
     expect(result).to_have_text('Ok')
 
 
-'''
-Напишите тест, который зайдет на страницу https://www.qa-practice.com/elements/new_tab/button,
-нажмет на кнопку Click, в открывшемся табе проверит,
-что в результате написано "I am a new page in a new tab" и проверит, что на изначальной вкладке кнопка Click - активна (enabled)
-'''
 def test_27_part2_tabs(page: Page, context: BrowserContext):
     page.set_viewport_size({'width': 1920, 'height': 1080})
     page.goto('https://www.qa-practice.com/elements/new_tab/button', wait_until='domcontentloaded')
@@ -38,10 +29,6 @@ def test_27_part2_tabs(page: Page, context: BrowserContext):
     expect(link).to_be_enabled()
 
 
-'''
-Напишите тест, который зайдет на страницу https://demoqa.com/dynamic-properties,
-нажмет на кнопку Color change только после того как она станет красной.
-'''
 def test_27_part3_waits(page: Page):
     page.set_viewport_size({'width': 1920, 'height': 1080})
     page.goto('https://demoqa.com/dynamic-properties', wait_until='domcontentloaded')
